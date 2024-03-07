0a6f4cb1156394fbc68a4bf90c6aca3571095332
AbouYounes <karim.khattou@univ-tiaret.dz>
2024-03-07 10:26:18 +0100

    Invoice & Article table added

--- invoice_app/models.py
+++ invoice_app/models.py
@@ -27,3 +27,44 @@ class Meta:
 
 def __str__(self):
     return self.name
+
+class Invoice(models.Model):
+    """
+    Name: Invoice model definition
+    Descriptio:
+    author: karim.khattou@univ-tiaret.dz
+    """
+    INVOICE_TYPE = (
+        ('R', 'RECIEVED'),
+        ('P', 'PROFORMA INVOICE'),
+        ('I', 'INVOICE')
+    )
+
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    saved_by = models.ForeignKey(User, on_delete=models.PROTECT)
+    invoice_date_time = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=100000, decimal_places=2)
+    last_updated_date = models.DateTimeField(null=True)
+    paid = models.BooleanField(default=False)
+    invoice_type = models.CharField(max_length=1, choices=INVOICE_TYPE)
+    comment = models.TextField(null=True, max_length=1000, blank=True)
+
+    class Meta:
+        verbose_name = "Invoice"
+        verbose_name_plural = "Invoices"
+
+    def __str__(self):
+        return f"{self.customer.name}_{self.invoice_date_time}"
+
+class Article(models.Model):
+    """
+    Name: Article model definition
+    Description:
+    Author: karim.khattou@univ-tiaret.dz
+    """        
+    
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    name = models.CharField(max_length=32)
+    quantity = model.IntegerFiled()
+    unit_price = models.DecimalField(max_digits=1000, decimal_palces=2)
+    total = models.DecimalField(max_digits=1000, decimal_palces=2)
